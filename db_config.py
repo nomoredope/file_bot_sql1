0ede387b21a9a9ea235523c7883b182aa1b75f50
nomodope <80510306+schenochek@users.noreply.github.com>
2022-10-12 13:03:58 +0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,4 +1,4 @@
-db_host = "127.0.0.1"
-db_user = "root"
-db_password = "Denis102"
-db_name = "file_bot_db"
+db_host = "your host_ip(if local: localhost or 127.0.0.1)"
+db_user = "your_username"
+db_password = "your_password"
+db_name = "data_base_name"
